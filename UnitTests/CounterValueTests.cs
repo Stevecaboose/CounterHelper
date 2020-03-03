0ccc6e58c7fcbc696524ab0bd42b4f766c57955d
Steven <stevenkurtscholz@gmail.com>
2020-03-02 20:51:17 -0600

    fixed folder structure

---
+++ UnitTests/CounterValueTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using CounterHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CounterValueTests
+    {
+        [TestMethod]
+        public void GetProcessorTime()
+        {
+            //Arrange
+            var option = new Options()
+            {
+                Name = "CPU Percent",
+                CategoryName = "Processor",
+                CounterName = "% Processor Time",
+                InstanceName = "_Total",
+                MachineName = ".",
+                ReadOnly = true,
+                IterationLength = 1000
+            };
+            ICounterManager counterHelper = new CounterManager(option);
+
+            //Act
+            counterHelper.GetCounterValue();
+            Thread.Sleep(1000);
+            var firstValue = float.Parse(counterHelper.GetCounterValue());
+            Thread.Sleep(1000);
+            var secondValue = float.Parse(counterHelper.GetCounterValue());
+
+            CounterManagerList.StopAllCounters();
+
+            // Assert
+            Assert.IsTrue(firstValue > 0 && secondValue > 0);
+
+        }
+
+    }
+}
